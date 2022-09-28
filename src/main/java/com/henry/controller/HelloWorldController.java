093794cbd69d3725cce4371227dad5bd4a8bd0de
Henry Xiloj <henry.xiloj@telusinternational.com>
2022-09-27 20:20:16 -0600

    first commit

--- src/main/java/com/henry/controller/HelloWorldController.java
+++ src/main/java/com/henry/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.henry.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloWorld(@PathVariable String name){
+        return "Hello "+ name;
     }
 }
 
