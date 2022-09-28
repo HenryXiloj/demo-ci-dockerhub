8fbca15af0c5a381c2cbfee40d678f207c39dfaa
Henry Xiloj <henry.xiloj@telusinternational.com>
2022-09-27 20:02:56 -0600

    first commit

---
+++ src/main/java/com/henry/DemoCiDockerhubApplication.java
@@ -0,0 +1,13 @@
+package com.henry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiDockerhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiDockerhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henry/DemoCiDockerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiDockerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
